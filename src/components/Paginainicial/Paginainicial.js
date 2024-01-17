538cba9267ff8e4fab602db0056f33ec205b28c8
ISABELLI CHRISPINIANO DE OLIVEIRA <isabelli.coliveira2@senacsp.edu.br>
2024-01-17 11:56:09 -0300

    novo projeto

---
+++ src/components/Paginainicial/Paginainicial.js
@@ -0,0 +1,27 @@
+import Logo from '../images/Logo.png';
+import './Paginainicial.css';
+
+function Paginainicial() {
+  return (
+    <>
+      <main>
+        <div>
+          <h1>Oi, me chamo Isabelli</h1>
+          <p>Sou desenvolvedora front-end</p>
+        </div>
+        <img src={Logo} alt='logo' className='img-logo' />
+      </main>
+      <footer>
+       <div className='textfooter'>
+         <button>Sobre mim</button>
+         <button>Meus projetos</button>
+         <button>Habilidades</button>
+         <button>Contato</button>
+       </div>
+      </footer>
+    </>
+  )
+}
+
+
+export default Paginainicial;
---
+++ src/components/Sobremim/Sobremim.js
@@ -0,0 +1,30 @@
+import Logo from '../images/Logo.png';
+import './Sobremim.css';
+
+
+function Sobremim(){
+    return(
+        <>
+         <header>
+            <div className='sobre'>
+            <img src={Logo} alt='Logo' className='Img-logo'/>
+            <h1>Sobre mim</h1>
+            </div>
+            <footer>
+                <h3 >Como um bruxo do mundo mágico, mergulhei nas profundezas encantadas da
+programação e do desenvolvimento web. Meu nome é Harry Potter, e assim como domino
+feitiços em Hogwarts, também domino as linguagens de programação. Com uma varinha e
+um teclado, conjuro códigos mágicos que transformam linhas de texto em experiências
+mágicas online. Da Sala Precisa aos servidores na nuvem, minha jornada como
+Desenvolvedor Full Stack é tão épica quanto derrotar Lord Voldemort. Desafios são como
+Horcruxes, e estou determinado a resolvê-los com a mesma coragem que usei para
+enfrentar as trevas. Lumos no código e avante, para construir um mundo digital repleto de
+possibilidades!</h3>
+            </footer>
+            
+         </header>
+        </>
+    )
+}
+
+export default Sobremim;
